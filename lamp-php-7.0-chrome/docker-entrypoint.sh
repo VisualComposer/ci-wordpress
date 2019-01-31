d872e04ba9832aa377239a0810faa67ff0ff276e
Vjaceslav Vasenin <time2type@gmail.com>
2019-01-31 17:21:48 +0200

    Disable supervisor foregound

--- lamp-php-7.0-chrome/docker-entrypoint.sh
+++ lamp-php-7.0-chrome/docker-entrypoint.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-set -m
-
-# Start apache2
-# /usr/sbin/apachectl -D FOREGROUND
-
-# Start mysql
-# service mysql start
-# exec mysqld_safe
+set -e
 exec supervisord -n
-
-# now we bring the primary process back into the foreground
-# and leave it there
-fg %1
