5720ff26af051d37138a1bea2635e2a6d1f2f1ef
Vjaceslav Vasenin <time2type@gmail.com>
2019-12-04 15:51:42 +0200

    Update auth plugin

--- circleci/vcv-auto-authorize.php
+++ circleci/vcv-auto-authorize.php
@@ -8,5 +8,13 @@ Author: VV - Visual Composer
 Author URI: http://visualcomposer.com
 */
 add_action('plugins_loaded', function () {
-    wp_set_current_user(1, true);
+    $user_login = 'admin';
+
+    $user = get_user_by( 'slug', $user_login );
+    $user_id = $user->ID;
+    
+    $curr = wp_set_current_user($user_id, $user_login);
+    error_log(print_r($curr,true));
+    wp_set_auth_cookie($user_id);
+    do_action('wp_login', $user_login, $user, false);
 });
